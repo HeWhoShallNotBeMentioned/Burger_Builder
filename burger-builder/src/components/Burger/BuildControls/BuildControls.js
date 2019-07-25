a1e3a6b8b2033ccd2e808a5d814958904f06d5d6
Chris Underwood <cunderwoodmn@gmail.com>
2019-07-25 15:11:10 -0700

    fixed folder naming errors. setup control for display in build controls and hooked them up to BurgerBuilder component for display

--- burger-builder/src/components/Burger/BuildControls/BuildControls.js
+++ burger-builder/src/components/Burger/BuildControls/BuildControls.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Meat', type: 'meat' },
+  { label: 'Cheese', type: 'cheese' },
+];
+
 const buildControls = props => {
-  return <div>Testing</div>;
+  return (
+    <div className={classes.BuildControls}>
+      {controls.map(ctrl => (
+        <BuildControl key={ctrl.label} label={ctrl.label} />
+      ))}
+    </div>
+  );
 };
 
 export default buildControls;
