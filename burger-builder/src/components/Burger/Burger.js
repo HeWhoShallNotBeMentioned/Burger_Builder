1f4454fb8c8793b86f5e6c34ad99cc5ab3276cab
Chris Underwood <cunderwoodmn@gmail.com>
2019-07-25 13:31:06 -0700

    add ability to have message when 0 ingredients are chosen.

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -3,13 +3,20 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = props => {
-  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredient key={igKey + i} type={igKey} />;
-    });
-  });
-  console.log('props...', props);
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
   console.log('transformedIngredients.........', transformedIngredients);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>`Please select ingredients.`</p>;
+  }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
