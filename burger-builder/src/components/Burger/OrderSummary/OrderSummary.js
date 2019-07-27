6ff141363fbf23b9f35bd3285a1b9400f96db2df
Chris Underwood <cunderwoodmn@gmail.com>
2019-07-26 17:28:50 -0700

    created modal and  backdrop

---
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import Aux from '../../../hoc/Aux';
+
+const orderSummary = props => {
+  const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
+    return (
+      <li key={igKey + 1}>
+        <span style={{ textTransform: 'capitalize' }}>{igKey}</span>:{' '}
+        {props.ingredients[igKey]}
+      </li>
+    );
+  });
+  console.log('ingredients Order Summary', ingredientSummary);
+  return (
+    <Aux>
+      <h3>Order Summary</h3>
+      <p>A delicious burger with the following ingredients:</p>
+      <ul>{ingredientSummary}</ul>
+      <p>Continue to checkout?</p>
+    </Aux>
+  );
+};
+
+export default orderSummary;
