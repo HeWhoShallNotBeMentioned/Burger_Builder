f16b49887cd4276a49ee4d7c7a7af14ad6d75c01
Chris Underwood <cunderwoodmn@gmail.com>
2019-07-28 18:55:29 -0700

    made the OrderSummary and Modal components classes. Set the modal and order summary to only update when new props are received

--- burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,33 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = props => {
-  const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
+class OrderSummary extends Component {
+  //This could be a functional component if constructor and component did update are removed
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidUpdate() {
+    console.log('[OrderSummary] DidUpdate');
+  }
+
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(igKey => {
+      return (
+        <li key={igKey + 1}>
+          <span style={{ textTransform: 'capitalize' }}>{igKey}</span>:{' '}
+          {this.props.ingredients[igKey]}
+        </li>
+      );
+    });
     return (
-      <li key={igKey + 1}>
-        <span style={{ textTransform: 'capitalize' }}>{igKey}</span>:{' '}
-        {props.ingredients[igKey]}
-      </li>
+      <Aux>
+        <h3>Order Summary</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>$ {this.props.total.toFixed(2)}</p>
+        <p>Continue to checkout?</p>
+        <Button btnType="Danger" clicked={this.props.clickedCancel}>
+          CANCEL
+        </Button>
+        <Button btnType="Success" clicked={this.props.clickedContinue}>
+          CONTINUE
+        </Button>
+      </Aux>
     );
-  });
+  }
+}
 
-  return (
-    <Aux>
-      <h3>Order Summary</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>{ingredientSummary}</ul>
-      <p>$ {props.total.toFixed(2)}</p>
-      <p>Continue to checkout?</p>
-      <Button btnType="Danger" clicked={props.clickedCancel}>
-        CANCEL
-      </Button>
-      <Button btnType="Success" clicked={props.clickedContinue}>
-        CONTINUE
-      </Button>
-    </Aux>
-  );
-};
-
-export default orderSummary;
+export default OrderSummary;
