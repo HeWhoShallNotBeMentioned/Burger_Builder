141ef0b427f2f2e433f0afe5f1585b1dbf70007f
Chris Underwood <cunderwoodmn@gmail.com>
2019-08-23 19:36:15 -0700

    retreived orders from firebase and formatter them in state

--- burger-builder/src/containers/Orders/Orders.js
+++ burger-builder/src/containers/Orders/Orders.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
 
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
+  state = { orders: [], loading: true };
+
+  async componentDidMount() {
+    try {
+      const { data } = await axios.get('/orders.json');
+      console.log('orders.data-----', data);
+      const fetchedOrders = [];
+      for (let key in data) {
+        fetchedOrders.push({ ...data[key], id: key });
+      }
+      this.setState({ loading: false, orders: fetchedOrders });
+    } catch (error) {
+      this.setState({ loading: false });
+    }
+  }
+
   render() {
     return (
       <div>
@@ -13,4 +31,4 @@ class Orders extends Component {
   }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
