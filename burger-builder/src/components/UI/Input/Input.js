63b9d9e17f4b6f06646d8d3de9f659eb0674257c
Chris Underwood <cunderwoodmn@gmail.com>
2019-08-25 16:08:26 -0700

    add visual validation for the end user

--- burger-builder/src/components/UI/Input/Input.js
+++ burger-builder/src/components/UI/Input/Input.js
@@ -4,11 +4,17 @@ import classes from './Input.css';
 
 const input = props => {
   let inputElement = null;
+  const InputClasses = [classes.InputElement];
+
+  if (props.invalid && props.shouldValidate) {
+    InputClasses.push(classes.Invalid);
+  }
+
   switch (props.elementType) {
     case 'input':
       inputElement = (
         <input
-          className={classes.InputElement}
+          className={InputClasses.join(' ')}
           {...props.elementConfig}
           value={props.value}
           onChange={props.changed}
@@ -18,7 +24,7 @@ const input = props => {
     case 'textarea':
       inputElement = (
         <textarea
-          className={classes.InputElement}
+          className={InputClasses.join(' ')}
           {...props.elementConfig}
           value={props.value}
           onChange={props.changed}
@@ -28,7 +34,7 @@ const input = props => {
     case 'select':
       inputElement = (
         <select
-          className={classes.InputElement}
+          className={InputClasses.join(' ')}
           value={props.value}
           onChange={props.changed}
         >
@@ -45,7 +51,7 @@ const input = props => {
     default:
       inputElement = (
         <input
-          className={classes.InputElement}
+          className={InputClasses.join(' ')}
           {...props.elementConfig}
           value={props.value}
           onChange={props.changed}
