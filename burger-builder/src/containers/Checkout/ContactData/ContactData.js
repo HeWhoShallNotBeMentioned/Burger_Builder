a195afca05bb495513e1d1b27eba6472c9f2b356
Chris Underwood <cunderwoodmn@gmail.com>
2019-08-25 16:29:02 -0700

    add ability to have fields only be tested for validity if they are touched

--- burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -21,6 +21,7 @@ class ContactData extends Component {
           minLength: 5,
         },
         valid: false,
+        touched: false,
       },
       street: {
         elementType: 'input',
@@ -33,6 +34,7 @@ class ContactData extends Component {
           required: true,
         },
         valid: false,
+        touched: false,
       },
       zipCode: {
         elementType: 'input',
@@ -47,6 +49,7 @@ class ContactData extends Component {
           maxLength: 5,
         },
         valid: false,
+        touched: false,
       },
       country: {
         elementType: 'input',
@@ -59,6 +62,7 @@ class ContactData extends Component {
           required: true,
         },
         valid: false,
+        touched: false,
       },
       email: {
         elementType: 'input',
@@ -71,6 +75,7 @@ class ContactData extends Component {
           required: true,
         },
         valid: false,
+        touched: false,
       },
       deliveryMethod: {
         elementType: 'select',
@@ -137,6 +142,7 @@ class ContactData extends Component {
       updatedFormElement.value,
       updatedFormElement.validation
     );
+    updatedFormElement.touched = true;
     console.log(updatedFormElement.valid);
     updatedOrderForm[inputIdentifier] = updatedFormElement;
     this.setState({ orderForm: updatedOrderForm });
@@ -159,6 +165,7 @@ class ContactData extends Component {
               value={elem.config.value}
               invalid={!elem.config.valid}
               shouldValidate={elem.config.validation}
+              touched={elem.config.touched}
               changed={event => this.inputChangedHandler(event, elem.id)}
             />
           );
