75605cae26bcb7511da3096e7f0a14202b3f1d61
Chris Underwood <cunderwoodmn@gmail.com>
2019-09-11 20:51:13 -0700

    set up post request to firebase for authentication.

--- burger-builder/src/Store/actions/auth.js
+++ burger-builder/src/Store/actions/auth.js
@@ -1,4 +1,8 @@
+import axios from 'axios';
+
 import { AUTH_START, AUTH_SUCCESS, AUTH_FAIL } from './actionTypes';
+
+require('dotenv').config();
 
 export const authStart = () => {
   return { type: AUTH_START };
@@ -19,7 +23,23 @@ export const authFail = error => {
 };
 
 export const fetchAuth = (email, password) => {
-  return dispatch => {
+  return async dispatch => {
     dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    };
+    try {
+      const { data } = await axios.post(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBzONL_c7wZAIqQIEk3dBqwzGaUl0VT8pg',
+        //process.env.FIREBASE_API_KEY,
+        authData
+      );
+      console.log('auth data from firebase   ', data);
+      dispatch(authSuccess(data));
+    } catch (error) {
+      dispatch(authFail(error));
+    }
   };
 };
