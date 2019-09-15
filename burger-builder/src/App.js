ac85427089998f1cfee27fb85f88bb4df1ee9f9b
Chris Underwood <cunderwoodmn@gmail.com>
2019-09-14 22:59:48 -0700

    add lazy loading to app.js routes

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
-import Logout from './containers/Auth/Logout/Logout';
+
+import Spinner from './components/UI/Spinner/Spinner';
 import { authCheckState } from './Store/actions/index';
-import Aux from './hoc/Aux/Aux';
+
+const Checkout = React.lazy(() => import('./containers/Checkout/Checkout'));
+const Orders = React.lazy(() => import('./containers/Orders/Orders'));
+const Auth = React.lazy(() => import('./containers/Auth/Auth'));
+const Logout = React.lazy(() => import('./containers/Auth/Logout/Logout'));
+
 class App extends Component {
   componentDidMount() {
     this.props.onTryAutoSignUp();
@@ -18,19 +21,22 @@ class App extends Component {
     return (
       <div>
         <Layout>
-          <Switch>
-            <Route path="/" exact component={BurgerBuilder} />
-            <Route path="/logout" component={Logout} />
-            <Route path="/auth" component={Auth} />
-            {this.props.isAuth ? (
-              <Aux>
-                <Route exact path="/orders" component={Orders} />
+          <React.Suspense fallback={<Spinner />}>
+            <Switch>
+              {this.props.isAuthenticated && (
+                <Route path="/logout" component={Logout} />
+              )}
+              <Route path="/auth" component={Auth} />
+              {this.props.isAuthenticated && (
                 <Route path="/checkout" component={Checkout} />
-              </Aux>
-            ) : (
-              <Route component={Auth} />
-            )}
-          </Switch>
+              )}
+              {this.props.isAuthenticated && (
+                <Route path="/orders" component={Orders} />
+              )}
+              <Route path="/" exact component={BurgerBuilder} />
+              <Redirect to="/" />
+            </Switch>
+          </React.Suspense>
         </Layout>
       </div>
     );
@@ -38,7 +44,7 @@ class App extends Component {
 }
 const mapStateToProps = state => {
   return {
-    isAuth: state.aut.token !== null,
+    isAuthenticated: state.aut.token !== null,
   };
 };
 
